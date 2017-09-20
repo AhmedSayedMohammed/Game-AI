30b524df1cd98361e8085d4a97c1de129fefa4bb
AhmedSayedMohammed <ahmed.baker5101995@gmail.com>
2017-09-20 06:30:03 +0200

    Add files via upload
    steps :
    1-create empty GameObject and add GlobalFlock script to it 
    2-create prefab then add Flock Script to it 
    3-FlockBorder is an additional script that make the flock runaway if the player distance is close from the GameObject add it on the empty GameObhect

---
+++ Flock.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flock : MonoBehaviour {
+	public Vector3 goalpos;
+	public static float runspeed=1;
+	public static float runrotationspeed=1;
+    //public GameObject target;
+	private float speed=1f;
+	private float rotationspeed=0.5f;
+	private float nieghbourdistance=3f;
+//	private Transform Target;
+	private Vector3 averageheading;
+	private Vector3 averageposition;
+
+	bool turning =false;
+
+	void Start ()
+	{
+      //  gflock = target.GetComponent<GlobalFlock>();
+		speed = Random.Range (1f, 5f)*runspeed;
+	}
+
+	void Update ()
+	{
+			if (Vector3.Distance (transform.position, goalpos) >= GlobalFlock.tankSize) {
+		turning = true;
+	} else {
+		turning = false;
+	}
+		if (turning) {
+					Vector3 direction = goalpos - transform.position;
+			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (direction),
+				rotationspeed * Time.deltaTime*runrotationspeed);
+			//when player is too close to the flock fiish will run 
+			speed = Random.Range (1f, 5f)*runspeed;
+		}
+			else {
+			if(Random.Range(0.5f,20)<1)
+				applayRules ();
+			}
+		//Always walk and avoid 
+		avoid ();
+		this.transform.Translate (0,0,speed*Time.deltaTime);
+	}
+	void avoid()
+	{
+
+		GameObject[] gos;
+		gos = GlobalFlock.allfish;
+		Vector3 vavoid = Vector3.zero;
+		float dist;
+		foreach (GameObject go in gos) {
+
+			if (go != this.gameObject) {
+				dist = Vector3.Distance (go.transform.position, this.transform.position);
+				if (dist < 5.0f) {
+
+					vavoid = vavoid + (this.transform.position - go.transform.position);
+					transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (vavoid), rotationspeed * Time.deltaTime*runrotationspeed);
+				}
+			
+			}
+		}	
+	}
+
+	void applayRules()
+	{
+	
+		GameObject[] gos;
+		gos = GlobalFlock.allfish;
+		Vector3 vcenter = Vector3.zero;
+		Vector3 vavoid = Vector3.zero;
+
+		float dist;
+		float gspeed = 0.1f;
+		int groupSize = 0;
+		foreach (GameObject go in gos) {
+		
+			if(go!=this.gameObject){
+				dist = Vector3.Distance (go.transform.position, this.transform.position);
+				if (dist <= nieghbourdistance) {
+				
+					vcenter += go.transform.position;
+					groupSize++;
+					if (dist < 2.0f) {
+					
+						vavoid = vavoid + (this.transform.position - go.transform.position);
+					//	transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (vavoid), rotationspeed * Time.deltaTime);
+					}
+					Flock anotherFlock = go.GetComponent<Flock> ();
+					gspeed = gspeed + anotherFlock.speed;
+				}
+
+			}
+		}
+			
+		if (groupSize > 0) {
+			vcenter = vcenter / groupSize + (goalpos - this.transform.position);
+			speed = gspeed / groupSize;
+			Vector3 direction = (vcenter + vavoid) - transform.position;
+			if (direction != goalpos) {
+				transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.LookRotation (direction), rotationspeed * Time.deltaTime*runrotationspeed);
+			
+			}
+		
+		}
+	}
+
+}
---
+++ GlobalFlock.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalFlock : MonoBehaviour {
+	public  GameObject   fishPref;
+	public  static float tankSize =10;
+	private static int   numFish  =10;
+	public  static GameObject [] allfish=new GameObject[numFish];
+    public GameObject Player;
+    private float timer = 0;
+    private bool is_inBorder = false;
+    // Use this for initialization
+    void Start () {
+
+		for (int i = 0; i < numFish; i++) {
+
+			Vector3 pos = new Vector3 (Random.Range (tankSize, tankSize+10), Random.Range (2, 3), Random.Range (tankSize, tankSize+10));
+			allfish[i]= (GameObject)Instantiate(fishPref,pos,Quaternion.identity);
+			allfish[i].transform.SetParent (this.transform,false);
+			float randomscale = Random.Range (5,10);
+			allfish [i].transform.localScale=new Vector3(randomscale,randomscale,randomscale);
+			allfish [i].transform.SetParent (null);
+
+            allfish[i].GetComponent<Flock>().goalpos = transform.position;
+                }
+	//Flock.goalpos = this.transform.position;
+		Debug.Log ("GlobalFlock goalpos"+tankSize);
+	}
+    void Update()
+    {
+        //Flock.goalpos = this.transform.position;
+        Border();
+        if (is_inBorder)
+        {
+            for (int i = 0; i < numFish; i++)
+                allfish[i].GetComponent<Flock>().goalpos = transform.position;
+        }
+       
+    }
+    void Border()
+    {
+
+        float x = Vector3.Distance(Player.transform.position, this.transform.position);
+        if (x <= 30)
+        {
+            this.transform.Translate(0, 0, 20);
+            timer = 4;
+            is_inBorder = true;
+        }
+        else { is_inBorder = false; }
+
+
+        if (timer >= 0)
+        {
+            Flock.runspeed = 5;
+            Flock.runrotationspeed = 5;
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            Flock.runspeed = 1;
+            Flock.runrotationspeed = 1;
+        }
+    }
+}
